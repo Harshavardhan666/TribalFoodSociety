594b05a1a10299c4a87149acd117532b943a2647
SATHI LAKSHMI PRIYATHA REDDY <priyareddy4774@gmail.com>
2023-09-29 04:31:13 +0530

    grouped in all_menu.php page

--- admin/all_menu.php
+++ admin/all_menu.php
@@ -6,9 +6,10 @@ error_reporting(0);
 session_start();
 
 ?>
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -23,49 +24,53 @@ session_start();
 
     <div class="preloader">
         <svg class="circular" viewBox="25 25 50 50">
-			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+        </svg>
     </div>
- 
+
     <div id="main-wrapper">
-      
-          <div class="header">
+
+        <div class="header">
             <nav class="navbar top-navbar navbar-expand-md navbar-light">
-            <div class="navbar-header">
+                <div class="navbar-header">
                     <a class="navbar-brand" href="dashboard.php">
-                        
+
                         <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
                     </a>
                 </div>
                 <div class="navbar-collapse">
-             
+
                     <ul class="navbar-nav mr-auto mt-md-0">
-                 
-                        
-                     
-                       
+
+
+
+
                     </ul>Admin
-                  
+
                     <ul class="navbar-nav my-lg-0">
 
-                        
-                   
+
+
                         <li class="nav-item dropdown">
-                           
+
                             <div class="dropdown-menu dropdown-menu-right mailbox animated zoomIn">
                                 <ul>
                                     <li>
                                         <div class="drop-title">Notifications</div>
                                     </li>
-                                    
+
                                     <li>
-                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all
+                                                notifications</strong> <i class="fa fa-angle-right"></i> </a>
                                     </li>
                                 </ul>
                             </div>
                         </li>
-                    
+
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-muted " style="padding: 0.5rem 0.5rem" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="images/bookingSystem/user-icn.png" alt="user" class="profile-pic" /></a>
+                            <a class="nav-link dropdown-toggle text-muted " style="padding: 0.5rem 0.5rem" href="#"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img
+                                    src="images/bookingSystem/user-icn.png" alt="user" class="profile-pic" /></a>
                             <div class="dropdown-menu dropdown-menu-right animated zoomIn">
                                 <ul class="dropdown-user">
                                     <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
@@ -76,134 +81,169 @@ session_start();
                 </div>
             </nav>
         </div>
-      
+
         <div class="left-sidebar">
-    
+
             <div class="scroll-sidebar">
-           
+
                 <nav class="sidebar-nav">
-                   <ul id="sidebarnav">
+                    <ul id="sidebarnav">
                         <li class="nav-devider"></li>
                         <li class="nav-label">Home</li>
                         <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
                         <li class="nav-label">Log</li>
-                        <li> <a href="all_users.php">  <span><i class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
-                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-archive f-s-20 color-warning"></i><span class="hide-menu">Departments</span></a>
+                        <li> <a href="all_users.php"> <span><i
+                                        class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i
+                                    class="fa fa-archive f-s-20 color-warning"></i><span
+                                    class="hide-menu">Departments</span></a>
                             <ul aria-expanded="false" class="collapse">
-								<li><a href="all_restaurant.php">All Departments</a></li>
-								<!-- <li><a href="add_category.php">Add village</a></li> -->
+                                <li><a href="all_restaurant.php">All Departments</a></li>
+                                <!-- <li><a href="add_category.php">Add village</a></li> -->
                                 <li><a href="add_restaurant.php">Add new Departments</a></li>
-                                
+
                             </ul>
                         </li>
-                     <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Items</span></a>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery"
+                                    aria-hidden="true"></i><span class="hide-menu">Items</span></a>
                             <ul aria-expanded="false" class="collapse">
-								<li><a href="all_menu.php">All Categories</a></li>
+                                <li><a href="all_menu.php">All Categories</a></li>
                                 <li><a href="add_foodCat.php">Add Item Category</a></li>
-								<li><a href="add_menu.php">Add Item</a></li>
-                              
-                                
+                                <li><a href="add_menu.php">Add Item</a></li>
+
+
                             </ul>
                         </li>
-						 <li> <a href="all_orders.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Orders</span></a></li>
-                         <li> <a href="reports.php"><i class="fa fa-file-text-o" aria-hidden="true"></i><span>Reports</span></a></li>
-                         
-                         <li> <a href="item_reports.php"><i class="fa fa-bar-chart" aria-hidden="true"></i><span>Items report</span></a></li>
+                        <li> <a href="all_orders.php"><i class="fa fa-shopping-cart"
+                                    aria-hidden="true"></i><span>Orders</span></a></li>
+                        <li> <a href="reports.php"><i class="fa fa-file-text-o"
+                                    aria-hidden="true"></i><span>Reports</span></a></li>
+
+                        <li> <a href="item_reports.php"><i class="fa fa-bar-chart" aria-hidden="true"></i><span>Items
+                                    report</span></a></li>
 
                     </ul>
                 </nav>
-        
-            </div>
-         
-        </div>
-     
-            <div class="page-wrapper">
-            
-                
-            
-                <div class="container-fluid">
-            
-                    <div class="row">
-                        <div class="col-12">
 
-                            <div class="col-lg-12">
+            </div>
+
+        </div>
+
+        <div class="page-wrapper">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="col-lg-12">
                             <div class="card card-outline-primary">
                                 <div class="card-header">
                                     <h4 class="m-b-0 text-white">All Products</h4>
                                 </div>
 
-                                    <div class="table-responsive m-t-40">
-                                        <table id="example23" class="display nowrap table table-hover table-striped table-bordered" cellspacing="0" width="100%">
-                                            <thead class="thead-dark">
+                                <div class="table-responsive m-t-40">
+
+
+                                    <?php
+
+                                    $departments = [];
+                                    $categories = [];
+                                    $categoryPrinted = [];
+                                    $categoryCounts = [];
+                                    $sql = "SELECT * FROM dishes order by d_id desc";
+                                    $query = mysqli_query($db, $sql);
+
+                                    if (!mysqli_num_rows($query) > 0) {
+                                        echo '<td colspan="11"><center>No Menu</center></td>';
+                                    } else {
+                                        while ($rows = mysqli_fetch_array($query)) {
+                                            if (!array_key_exists($rows['rs_id'], $departments)) {
+                                                $departments[$rows['rs_id']] = [];
+                                            }
+
+                                            if (!array_key_exists($rows['fc_id'], $categories)) {
+                                                $categories[$rows['fc_id']] = [];
+                                            }
+
+                                            $departments[$rows['rs_id']][$rows['fc_id']][] = $rows;
+                                        }
+                                    }
+
+                                    $sql = "SELECT fc_id, COUNT(*) AS product_count FROM dishes GROUP BY fc_id";
+                                    $query = mysqli_query($db, $sql);
+
+                                    while ($row = mysqli_fetch_array($query)) {
+                                        $categoryCounts[$row['fc_id']] = $row['product_count'];
+                                    }
+                                    // Print all of the tables.
+                                    foreach ($departments as $departmentId => $departmentRows) {
+                                        echo '<h2>Department: ' . $departmentId . '</h2>';
+
+
+
+                                        foreach ($categories as $categoryId => $categoryRows) {
+                                            // echo '<h3>Category: ' . $categoryId . '</h3>';
+                                    
+                                            echo '  <table id="example23"
+                                            class="display nowrap table table-hover table-striped table-bordered"
+                                            cellspacing="0" width="100%"> ';
+                                            echo '<thead class="thead-dark">
                                                 <tr style="text-align: center;">
-                                                <th>Department</th>
+                                                    <th>Category</th>
                                                     <th>Item</th>
                                                     <th>Description</th>
                                                     <th>Price</th>
-                                                    <!-- <th>Calories</th> -->
+    
                                                     <th>Image</th>
-                                                <th>Action</th>	  
+                                                    <th>Action</th>
                                                 </tr>
-                                            </thead>
-                                            <tbody>
-                                            
-                                                    <?php
-                                                    $sql="SELECT * FROM dishes order by d_id desc";
-                                                    $query=mysqli_query($db,$sql);
-                                                    
-                                                        if(!mysqli_num_rows($query) > 0 )
-                                                            {
-                                                                echo '<td colspan="11"><center>No Menu</center></td>';
-                                                            }
-                                                        else
-                                                            {				
-                                                                        while($rows=mysqli_fetch_array($query))
-                                                                            {
-                                                                                    $mql="select * from restaurant where rs_id='".$rows['rs_id']."'";
-                                                                                    $newquery=mysqli_query($db,$mql);
-                                                                                    $fetch=mysqli_fetch_array($newquery);
-                                                                                    
-                                                                                    
-                                                                                        echo '<tr><td>'.$fetch['title'].'</td>
-                                                                                        
-                                                                                                    <td>'.$rows['title'].'</td>
-                                                                                                    <td>'.$rows['slogan'].'</td>
-                                                                                                    <td>Rs '.$rows['price'].'</td>  
-                                                                                                    <td><div class="col-md-3 col-lg-8 m-b-10">
-                                                                                                    <img src="Res_img/dishes/'.$rows['img'].'" class="img-responsive  radius" style="min-width:150px;min-height:100px;" />
-                                                                                                    </div></td>
-                                                                                                    
-                                                                                                
-                                                                                                        <td><a href="delete_menu.php?menu_del='.$rows['d_id'].'" class="btn btn-danger btn-flat btn-addon btn-xs m-b-10"><i class="fa fa-trash-o" style="font-size:16px"></i></a> 
-                                                                                                        <a href="update_menu.php?menu_upd='.$rows['d_id'].'" class="btn btn-info btn-flat btn-addon btn-sm m-b-10 m-l-5"><i class="fa fa-edit"></i></a>
-                                                                                                        </td></tr>';
-                                                                                        
-                                                                                            
-                                                                                            
-                                                                            }	
-                                                            }
-                                                ?>            
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            </div>
-                        
+                                            </thead>';
+                                            echo '<tbody>';
+
+                                            foreach ($departmentRows[$categoryId] as $row) {
+                                                echo '<tr>';
+                                                // echo '<td col-span=3>Category: ' . $categoryId . ' </td>';
+                                                if (!in_array($categoryId, $categoryPrinted)) {
+                                                    echo '<td style="text-align: center; vertical-align: middle;" rowspan=' . $categoryCounts[$categoryId] . '>Category: ' . $categoryId . ' </td>';
+
+
+                                                    $categoryPrinted[] = $categoryId;
+                                                }
+                                                echo '<td>' . $row['title'] . '</td>';
+                                                echo '<td>' . $row['slogan'] . '</td>';
+                                                echo '<td>' . $row['price'] . '</td>';
+                                                echo '<td><div class="col-md-3 col-lg-8 m-b-10">
+                                                    <img src="Res_img/dishes/' . $rows['img'] . '" class="img-responsive  radius" style="min-width:150px;min-height:100px;" />';
+                                                echo '<td><a href="delete_menu.php?menu_del=' . $rows['d_id'] . '" class="btn btn-danger btn-flat btn-addon btn-xs m-b-10"><i class="fa fa-trash-o" style="font-size:16px"></i></a> 
+                                                    <a href="update_menu.php?menu_upd=' . $rows['d_id'] . '" class="btn btn-info btn-flat btn-addon btn-sm m-b-10 m-l-5"><i class="fa fa-edit"></i></a>
+                                                    </td>';
+                                                echo '</tr>';
+                                            }
+
+                                            echo '</tbody></table>';
+                                        }
+                                    }
+                                    ?>
+
                                 </div>
                             </div>
                         </div>
+
                     </div>
-                
+
                 </div>
-            
-                <footer class="footer"> © 2023 - TribalFoodSociety </footer>
-            
             </div>
-       
+        </div>
     </div>
-    
+
+    </div>
+
+    <footer class="footer"> © 2023 - TribalFoodSociety </footer>
+
+    </div>
+
+    </div>
+
     <script src="js/lib/jquery/jquery.min.js"></script>
     <script src="js/lib/bootstrap/js/popper.min.js"></script>
     <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
