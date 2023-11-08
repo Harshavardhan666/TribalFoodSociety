c95a64f2c98bc76966e1a6f3933868bf1c04641e
yash664 <yashoosworld@gmail.com>
2023-11-08 15:07:26 +0530

    order placing issue resolved

--- checkout.php
+++ checkout.php
@@ -6,80 +6,33 @@ include_once 'product-action.php';
 error_reporting(0);
 session_start();
 
-?>
 
-<script>
-    function showModal(content) {
-        // console.log("Showing modal with message:", content);
-        document.getElementById("modalContent").textContent = content;
-        document.getElementById("myModal").style.display = "block";
-    }
-
-    function redirectToPage(delayInMilliseconds) {
-        settime(delayInMilliseconds);
-    }
-
-    function settime(delayInMilliseconds) {
-        setTimeout(page, delayInMilliseconds);
-    }
-
-    function page() {
-        window.location.href = "your_orders.php";
-    }
-</script>
-
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <!-- <span class="close" onclick="hideModal();">&times;</span> -->
-        <!-- <div class="modal-icon modal-success"><i class="fa-solid fa-badge-check fa-2xs"></i></div> -->
-
-        <p id="modalContent">Modal Content</p>
-    </div>
-</div>
-
-<?php
-
-// function function_alert()
-// {
-
-
-//     echo "<script>alert('Thank you. Your Order has been placed!');</script>";
-//     echo "<script>window.location.replace('your_orders.php');</script>";
-// }
+function function_alert()
+{
+    echo "<script>alert('Thank you. Your Order has been placed!');</script>";
+    echo "<script>window.location.replace('your_orders.php');</script>";
+}
 
 if (empty($_SESSION["user_id"])) {
     header('location:login.php');
 } else {
-
-
     foreach ($_SESSION["cart_item"] as $item) {
 
         $item_total += ($item["price"] * $item["quantity"]);
 
         if ($_POST['submit']) {
             $session = $_SESSION["user_id"];
-            $user = mysqli_query($db, "select balance from users where u_id='$session' ");
-            $rows = mysqli_fetch_array($user);
-            if ((($rows["balance"] - $item_total) >= 0)) {
-                $bal = $rows["balance"] - $item_total;
-                $SQL = "UPDATE users SET balance=$bal WHERE u_id='" . $_SESSION["user_id"] . "'";
-                mysqli_query($db, $SQL);
-
-                $SQL = "insert into users_orders(u_id,title,quantity,price) values('" . $_SESSION["user_id"] . "','" . $item["title"] . "','" . $item["quantity"] . "','" . $item["price"] . "')";
-                mysqli_query($db, $SQL);
-                unset($_SESSION["cart_item"]);
-                unset($item["title"]);
-                unset($item["quantity"]);
-                unset($item["price"]);
-                $success = "Thank you. Your order has been placed!";
-                // showModal($success);
-                echo '<script>showModal("' . $success . '"); redirectToPage(1000);</script>';
-            } else {
-                echo "<script>alert('Insufficient Balance');</script>";
-            }
+            $SQL = "insert into users_orders(u_id,title,quantity,price) values('" . $_SESSION["user_id"] . "','" . $item["title"] . "','" . $item["quantity"] . "','" . $item["price"] . "')";
+            mysqli_query($db, $SQL);
+            unset($_SESSION["cart_item"]);
+            unset($item["title"]);
+            unset($item["quantity"]);
+            unset($item["price"]);
+            $success = "Thank you. Your order has been placed!";
+            function_alert();
         }
     }
-    ?>
+?>
 
 
     <head>
@@ -88,99 +41,15 @@ if (empty($_SESSION["user_id"])) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="icon" type="image/x-icon" href="images/tribes2.ico">
+        <link rel="icon" href="#">
         <title>Checkout</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/font-awesome.min.css" rel="stylesheet">
         <link href="css/animsition.min.css" rel="stylesheet">
         <link href="css/animate.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
-
-        <!-- <script type="text/javascript">
-
-        function check()
-        {
-            var rd1 = document.getElementById("radioStacked1");
-
-            if(rd1.checked == false)
-            {
-                alert("Please Select Valid Payment Method");
-            }else if (rd1.checked == true)
-            {
-                <p class="text-xs-center"> <input type="submit" onclick="return confirm('Do you want to confirm the order?');" name="submit"  class="btn btn-success btn-block" value="Order Now"> </p>
-            }
-        }
-
-    </script> -->
-
-        <!-- <style>
-        .btn{
-            display: none;
-        }
-
-        #radioStacked1:checked + .btn{
-            display: block;
-        }
-
-    </style> -->
-
         <style>
-            .modal {
-                display: none;
-                position: fixed;
-                z-index: 1;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                overflow: auto;
-                background-color: rgba(0, 0, 0, 0.6);
-            }
-
-            .modal-content {
-                background-color: #f9f9f9;
-                margin: 10% auto;
-                padding: 20px;
-                border: 1px solid #ccc;
-                width: 80%;
-                max-width: 400px;
-                border-radius: 8px;
-                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
-            }
-
-            .modal-content p {
-                margin: 0;
-                font-size: 18px;
-                line-height: 1.5;
-                color: #333;
-            }
-
-            .modal-content p:first-of-type {
-                margin-bottom: 20px;
-            }
-
-            .modal-content p:last-of-type {
-                text-align: center;
-            }
-
-            .modal-content p:last-of-type a {
-                color: #007bff;
-                text-decoration: none;
-            }
-
-            .modal-content p:last-of-type a:hover {
-                text-decoration: underline;
-            }
-
-            .nav-item.dropdown:hover .dropdown-menu {
-                display: block;
-            }
-
-            .dropdown-item:hover {
-                background-color: #abcdef;
-            }
-
-            .order-table {
+                        .order-table {
                 width: 100%;
                 border-collapse: collapse;
             }
@@ -194,7 +63,7 @@ if (empty($_SESSION["user_id"])) {
 
             .order-table th {
                 background-color: #f2f2f2;
-            }
+            } 
         </style>
     </head>
 
@@ -204,24 +73,17 @@ if (empty($_SESSION["user_id"])) {
             <header id="header" class="header-scroll top-header headrom">
                 <nav class="navbar navbar-dark">
                     <div class="container">
-                        <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse"
-                            data-target="#mainNavbarCollapse">&#9776;</button>
-                        <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/tribes.ico" alt=""
-                                width="100" height="40"> </a>
+                        <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                        <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/tribes.ico" alt="" width="100" height="40"> </a>
                         <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                             <ul class="nav navbar-nav">
-                                <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span
-                                            class="sr-only">(current)</span></a> </li>
-                                <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Departments <span
-                                            class="sr-only"></span></a> </li>
-                                <li class="nav-item"> <a class="nav-link active" href="edit_profile.php">Profile <span
-                                            class="sr-only"></span></a> </li>
+                                <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                                <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Departments <span class="sr-only"></span></a> </li>
+                                <li class="nav-item"> <a class="nav-link active" href="edit_profile.php">Profile <span class="sr-only"></span></a> </li>
                                 <!-- <li class="nav-item"> <a class="nav-link active" href="">About <span class="sr-only"></span></a> </li> -->
 
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                                        style="color: white;">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
                                         About
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -253,11 +115,9 @@ if (empty($_SESSION["user_id"])) {
                     <div class="container">
                         <ul class="row links">
 
-                            <li class="col-xs-12 col-sm-4 link-item"><span>1</span><a href="restaurants.php">Choose
-                                    Department</a></li>
-                            <li class="col-xs-12 col-sm-4 link-item "><span>2</span><a href="#">Pick Your Product</a></li>
-                            <li class="col-xs-12 col-sm-4 link-item active"><span>3</span><a href="checkout.php">Order and
-                                    Pay</a></li>
+                            <li class="col-xs-12 col-sm-4 link-item"><span>1</span><a href="restaurants.php">Choose Canteen</a></li>
+                            <li class="col-xs-12 col-sm-4 link-item "><span>2</span><a href="#">Pick Your favorite food</a></li>
+                            <li class="col-xs-12 col-sm-4 link-item active"><span>3</span><a href="checkout.php">Order and Pay</a></li>
                         </ul>
                     </div>
                 </div>
@@ -265,7 +125,7 @@ if (empty($_SESSION["user_id"])) {
                 <div class="container">
 
                     <span style="color:green;">
-                        <?php echo '<script>showModal($success); redirectToPage(1000);</script>' ?>
+                        <?php echo $success; ?>
                     </span>
 
                 </div>
@@ -281,17 +141,11 @@ if (empty($_SESSION["user_id"])) {
                                 <form method="post" action="#">
                                     <div class="row">
 
-
-
                                         <div class="col-sm-12">
                                             <div class="cart-totals margin-b-20">
                                                 <div class="cart-totals-title">
                                                     <h4>Cart Summary</h4>
                                                 </div>
-                                                <!-- <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3"> -->
-                                                <!-- <div class="widget widget-cart"> -->
-
-                                                <!-- <div class="order-row bg-white"> -->
                                                 <div class="widget-body">
                                                     <table class="order-table">
                                                         <thead>
@@ -347,71 +201,24 @@ if (empty($_SESSION["user_id"])) {
                                                     <!-- </div> -->
 
                                                 </div>
-                                                <!-- </div> -->
-
-
-
-
-
-
-
-
-
                                             </div>
 
-
-
-
-
-
-
-
-
-
-
-                                        </div>
-
-                                        <div class="payment-option">
-                                            <ul class=" list-unstyled">
-                                                <div class="cart-totals-title">
-                                                    <h4>Payment Methods</h4>
-                                                </div>
-                                                <hr style="width:100%;text-align:left;margin-left:0">
-                                                <li>
-                                                    <label for="radioStacked1" class="custom-control custom-radio  m-b-20">
-                                                        <input name="Amrita wallet" id="radioStacked1" checked
-                                                            value="Amrita wallet" type="radio" class="custom-control-input">
-                                                        <span class="custom-control-indicator"></span> <span
-                                                            class="custom-control-description">Cash on Delivery (COD)</span>
-                                                    </label>
-                                                </li>
-                                                <!-- <div class="cart-totals-fields">
-                                                        <table class="table">
-                                                            <tbody>
-                                                               
-                                                                
-                                                            </tbody>
-                                                        </table>
-                                                    </div> -->
-                                                <!-- <li>
-                                                <label class="custom-control custom-radio  m-b-10">
-                                                    <input name="mod"  type="radio" value="paypal" disabled class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Paypal <img src="images/paypal.jpg" alt="" width="90"></span> </label>
-                                            </li> -->
-                                            </ul>
-
-                                            <!-- <form class="form-horizontal" role="form" action="" method="post" id="myForm">
-                                                    <div class="col-md-4">
-                                                        <label class="control-label">E-Wallet PIN:</label>
-                                                        <input class="form-control" type="password" value="****" max=4 name="pin" required>
+                                            <div class="payment-option">
+                                                <ul class=" list-unstyled">
+                                                    <div class="cart-totals-title">
+                                                        <h4>Payment Methods</h4>
                                                     </div>
-                                                </form> -->
+                                                    <hr style="width:100%;text-align:left;margin-left:0">
+                                                    <li>
+                                                        <label for="radioStacked1" class="custom-control custom-radio  m-b-20">
+                                                            <input name="Amrita wallet" id="radioStacked1" checked value="Amrita wallet" type="radio" class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Cash On delivery</span>
+                                                        </label>
+                                                    </li>
 
 
-                                            <p class="text-xs-center"> <input type="submit"
-                                                    onclick="return confirm('Do you want to confirm the order?');"
-                                                    name="submit" class="btn btn-success btn-block" value="Order Now"> </p>
-                                        </div>
-
+                                                </ul>
+                                                <p class="text-xs-center"> <input type="submit" onclick="return confirm('Do you want to confirm the order?');" name="submit" class="btn btn-success btn-block" value="Order Now"> </p>
+                                            </div>
                                 </form>
                             </div>
                         </div>
@@ -426,8 +233,7 @@ if (empty($_SESSION["user_id"])) {
                 <div class="container">
                     <div class="row">
 
-                        <a href="" target="_blank"><img src="images/masinagudi.jpg"
-                                class="col-xs-12 col-sm-3 payment-options color-gray"></a>
+                        <a href="" target="_blank"><img src="images/masinagudi.jpg" class="col-xs-12 col-sm-3 payment-options color-gray"></a>
 
                         <div class="col-xs-12 col-sm-4 address color-gray">
                             <h5>Address</h5>
@@ -458,8 +264,8 @@ if (empty($_SESSION["user_id"])) {
         <script src="js/foodpicky.min.js"></script>
     </body>
 
-    </html>
+</html>
 
-    <?php
+<?php
 }
 ?>
