d42ed9bb3bca4c8da53da674b1e8dd057c0e206f
Harshavardhan666 <tadikonda.harshavardhan24@gmail.com>
2023-11-19 13:05:19 +0530

    Made Footers Responsive

--- edit.php
+++ edit.php
@@ -235,11 +235,11 @@ if (isset($_POST['submit'])) {
       color: #bbbbbb;
     }
 
-    .faicons a:hover{
-            color: #0000FF;
-        }
+    .faicons a:hover {
+      color: #0000FF;
+    }
 
-    .icon{
+    .icon {
       font-size: 20px;
     }
 
@@ -662,59 +662,62 @@ if (isset($_POST['submit'])) {
 
       <footer class="footer">
         <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-3" style="margin-left: 0px;">
+              <a href="#" target="_blank">
+                <img src="images/masinagudi.jpg" class="img-responsive" alt="Masinagudi Image" style="height: 180px; width: 100%; margin-bottom: 10px;">
+              </a>
+            </div>
 
-          <div class="row bottom-footer">
-            <div class="container">
-              <div class="rowss">
-                <!-- <div class="col-xs-12 col-sm-3 payment-options color-gray">
-                              <h5>Payment Options</h5>
-                              <ul>
-                                 <li>
-                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
-                                 </li>
-                                 <li>
-                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
-                                 </li>
-                                 <li>
-                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
-                                 </li>
-                                 <li>
-                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
-                                 </li>
-                                 <li>
-                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
-                                 </li>
-                              </ul>
-                           </div> -->
-
-                <a href="" target="_blank"><img src="images/masinagudi.jpg" class="col-xs-12 col-sm-3 payment-options color-gray" style="width: 100%; height: 190px;"></a>
-
-                <div class="col-xs-12 col-sm-1 address color-gray">
-                  <h5 style="text-align: center; margin-top: 0;">Address</h5>
-                  <p style="text-align: center; margin-top: 0;">Masinagudi Village, Tribal Cooperative Society building, Near Ooty Main Town, PIN: 643223</p>
+            <div class="col-xs-12 col-sm-8">
+              <div class="row">
+                <div class="col-xs-12 col-sm-4 address">
+                  <h5 class="text-center">Address</h5>
+                  <p class="text-center">Masinagudi Village, Tribal Cooperative Society building, Near Ooty Main Town, PIN: 643223</p>
                 </div>
-                <div class="col-xs-12 col-sm-2 additional-info color-gray">
-                  <h5 style="text-align: center; margin-top: 0;">Contact</h5>
-                  <!-- <p>Join thousands of other restaurants who benefit from having partnered with us.</p> -->
-                  <p style="text-align: center; margin-top: 0;">Tribal Research Center, Nanjanad Road, Muttorai Palada, Ooty, Tamil Nadu, 634004, India</p>
 
-                  <div class="faicons">
+                <div class="col-xs-12 col-sm-5 additional-info">
+                  <h5 class="text-center">Contact</h5>
+                  <p class="text-center">Tribal Research Center, Nanjanad Road, Muttorai Palada, Ooty, Tamil Nadu, 634004, India</p>
+
+                  <div class="faicons text-center">
                     <a href="#" class="fa fa-facebook icon"></a>
                     <a href="#" class="fa fa-twitter icon"></a>
                     <a href="#" class="fa fa-instagram icon"></a>
                     <a href="#" class="fa fa-linkedin icon"></a>
                     <a href="#" class="fa fa-youtube icon"></a>
                   </div>
-
                 </div>
-                <a href="" target="_blank"><img src="images/TRC.jpg" alt="TRC" style="width: 100%; height: 165px; margin-top: 10px;"></a>
 
+                <div class="col-xs-12 col-sm-3">
+                  <a href="#" target="_blank">
+                    <img src="images/TRC.jpg" class="img-responsive" alt="TRC Image" style="height: 165px; width: 100%;">
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-
         </div>
       </footer>
+
+      <style>
+        /* Adjust styles for smaller screens */
+        @media (max-width: 767px) {
+
+          .address,
+          .additional-info,
+          .faicons {
+            margin-top: 10px;
+          }
+
+          .faicons a {
+            font-size: 24px;
+            /* Adjust icon size for better visibility */
+            margin-right: 10px;
+            /* Add space between icons */
+          }
+        }
+      </style>
 
     </div>
 
